89d50bd5902b750edc536004fe33cf8fd77f2967
Rafael1616 <sd0058032023@unab.edu.sv>
2025-03-06 16:36:26 -0600

    Guia 5: Conexion con la base de datos

---
+++ pVenta/src/database/pruebaConexion.java
@@ -0,0 +1,16 @@
+
+package database;
+
+
+public class pruebaConexion {
+    public static void main(String[] args){
+        Conexion con = new Conexion();
+        con.conectar();
+        if(con.connection != null){
+            System.out.println("Conectado");
+        }
+        else{
+            System.out.println("Sin conexion");
+        }
+    }
+}
